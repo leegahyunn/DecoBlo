c7f2a726a75616159689cd93eec6057a8a53ed3d
owner\USER <jungminn@hanmail.net>
2018-08-30 22:11:24 +0900

    변경 사항: 메뉴 타이틀 수정기능(메소드추가), 메뉴 불러오기 기능, 메뉴설정 드래그앤드롭 수정
    변경 이유: 기능추가, 기능수정

--- src/main/java/com/decoblog/www/blog/dao/BlogMapper.java
+++ src/main/java/com/decoblog/www/blog/dao/BlogMapper.java
@@ -8,5 +8,7 @@ import com.decoblog.www.blog.vo.Menu;
 public interface BlogMapper {
 	// 전체 메뉴 가져오기
 	public ArrayList<HashMap<String, ArrayList<Menu>>> selectMenu();
+	// 메뉴 타이틀 수정
+	public int updateMenu(Menu menu);
 
 }
